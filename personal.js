09c6ce0d09b40bf8f44c97d865176b16c74a1133
patteesolomon <theshiningkillerrashakublade@outlook.com>
2023-03-26 16:22:16 -0500

    Add files via upload
    First Commit.

---
+++ personal.js
@@ -0,0 +1,6 @@
+var doc = document;
+
+let dat = doc.getElementById('data');
+
+dat.innerHTML = 'Personal Site';
+dat.style.fontSize = '2em';
---
+++ readme.md
@@ -0,0 +1,4 @@
+#Personal Website#
+
+- Description
+This is an assignment for the course Web Development at Full Stack Academy.
